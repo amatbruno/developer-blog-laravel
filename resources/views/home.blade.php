17e16e840c65c04d5fa27204918b9a257b5aeca6
Bruno <121259934+amatbruno@users.noreply.github.com>
2024-03-07 11:30:28 +0100

    Responsive Design Homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,30 +28,104 @@
         svg {
             stroke: white;
         }
+
+
+        /*MEDIA QUERIES*/
+        /* Media query for mobile screens */
+        @media only screen and (max-width: 767px) {
+            #background-red {
+                height: 500px;
+            }
+            
+            #container-blur {
+                display: flex;
+                flex-direction: column;
+                justify-content: start;
+                align-items: center;
+                width: 500px;
+                height: fit-content;
+                padding-bottom: 20px;
+                padding-top: 20px;
+                gap: 2.5rem;
+            }
+
+            #container-blur .mini-container img {
+                width: 180px
+            }
+            
+            #container-blur .mini-container h1 {
+                font-size: 20px;
+                width: 240px;
+                padding-top: 30px;
+                padding-bottom: 30px;
+            }
+
+            #container-blur .mini-container a {
+                font-size: 19px
+            }
+        }
+
+        /* Media query for tablets in landscape mode */
+        @media only screen and (min-width: 768px) and (max-width: 1023px) {
+            #container-blur {
+                display: grid;
+                grid-template-columns: auto auto;
+                gap: 2rem;
+                width: 820px;
+                height: fit-content;
+                padding: 20px;
+            }
+
+            #container-blur .mini-container {
+                width: 360px;
+                
+            }
+
+            #container-blur .mini-container img {
+                width: 150px
+            }
+
+            #container-blur .mini-container h1 {
+                font-size: 19px;
+                width: 230px;
+            }
+        }
+
+        /* Media query for desktop */
+        @media only screen and (min-width: 1024px) {
+            #container-blur .mini-container {
+                width: 360px;
+            }
+
+            #container-blur {
+                width: 1200px;
+            }
+        }
+
     </style>
 </head>
 
-<body class="antialiased overflow-hidden">
-    <main class="bg-red-800 h-[420px]">
+<body class="antialiased">
+    <main id="background-red" class="bg-red-800 h-[420px]">
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
-            <h1 class="text-8xl text-white font-bold text-center">Developers Blog</h1>
-            <p class="text-white text-3xl text-center mt-16">Discover. Inspire. Thrive.</p>
+            <h1 class="text-7xl text-white font-bold text-center">DEVELOPERS BLOG</h1>
+            <p class="text-white text-2xl text-center mt-12">Discover. Inspire. Thrive.</p>
         </div>
-        <div id="container-blur" class="w-[1550px] h-[650px] m-auto mt-6 rounded-xl flex justify-center items-center gap-20">
-            <div class="mini-container w-[410px] h-[550px] rounded-xl flex flex-col justify-center items-center p-7">
-                <img class="w-62" src="https://img.icons8.com/external-itim2101-lineal-itim2101/256/FFFFFF/external-blog-copywriting-itim2101-lineal-itim2101.png" alt="blog_img">
-                <h1 class="text-white text-center text-2xl py-5 w-68">Create your own post or just see what peaople is inspired with</h1>
-                <a class="text-white my-3 text-lg mt-5 bg-red-600 p-3 px-6 rounded-lg" href="{{ url('posts') }}">Know More</a>
+        <div id="container-blur" class="h-[600px] m-auto mt-5 rounded-xl flex justify-center items-center gap-8">
+            <div class="mini-container h-[530px] rounded-xl flex flex-col justify-center items-center p-7">
+                <img class="w-44" src="https://img.icons8.com/external-itim2101-lineal-itim2101/256/FFFFFF/external-blog-copywriting-itim2101-lineal-itim2101.png" alt="blog_img">
+                <h1 class="text-white text-center text-2xl py-5 w-[290px]">Create your own post or just see what peaople is inspired with</h1>
+                <a class="text-white my-3 text-lg mt-5 bg-red-600 p-3 px-6 rounded-lg" href="{{ url('posts') }}">Post Feed</a>
             </div>
-            <div class="mini-container w-[410px] h-[550px] rounded-xl flex flex-col justify-center items-center p-7">
-                <img class="w-62" src="https://img.icons8.com/ios/256/FFFFFF/news.png" alt="blog_img">
-                <h1 class="text-white text-center text-2xl py-5 w-68">Explore the recent services and the upcoming features.</h1>
-                <button class="text-white my-3 text-lg mt-5 bg-red-600 p-3 px-6 rounded-lg" onclick="{{ url('') }}">Know More</button>
+            <div class="mini-container h-[530px] rounded-xl flex flex-col justify-center items-center p-7">
+                <img class="w-44" src="https://img.icons8.com/ios/256/FFFFFF/news.png" alt="blog_img">
+                <h1 class="text-white text-center text-2xl py-5 w-[290px]">Explore the recent services and the upcoming features.</h1>
+                <a class="text-white my-3 text-lg mt-5 bg-red-600 p-3 px-6 rounded-lg" onclick="{{ url('') }}">Blog News</a>
             </div>
-            <div class="mini-container w-[410px] h-[550px] rounded-xl flex flex-col justify-center items-center p-7">
-                <img class="w-62" src="https://img.icons8.com/wired/256/FFFFFF/name.png" alt="blog_img">
-                <h1 class="text-white text-center text-2xl py-5 w-68">Create your own post or just see what peaople is inspired with</h1>
-                <button class="text-white my-3 text-lg mt-5 bg-red-600 p-3 px-6 rounded-lg" onclick="{{ url('') }}">Know More</button>
+            <div class="mini-container h-[530px] rounded-xl flex flex-col justify-center items-center p-7">
+                <img class="w-44" src="https://img.icons8.com/wired/256/FFFFFF/name.png" alt="blog_img">
+                <h1 class="text-white text-center text-2xl py-5 w-[200px]">Manage posts and customize your profile</h1>
+                <a class="text-white my-3 text-lg mt-5 bg-red-600 p-3 px-6 rounded-lg" onclick="{{ url('') }}">User Area</a>
             </div>
         </div>
     
@@ -71,8 +145,8 @@
         @endif --}}
         
     </div>
-        <div class="flex justify-center mt-12 px-0">
-            <div class="text-center text-lg text-gray-100 dark:text-gray-200 sm:text-right sm:ml-0">
+        <div class="flex justify-center mt-12  px-0">
+            <div class="text-center text-lg text-gray-100 dark:text-gray-200 sm:text-right sm:ml-0 mb-12">
                 <p>Laravel v{{ Illuminate\Foundation\Application::VERSION }} | Made by Bruno Amat</p>
             </div>
         </div>
